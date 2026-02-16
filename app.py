9fa4d5babb65a9dca5bd80f89722dfb60e0c776c
Vijaykumar-03 <paxsaat03@gmail.com>
2026-02-16 13:34:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ import json, boto3, urllib.request
 from datetime import datetime
 
 dynamodb = boto3.resource('dynamodb')
-table = dynamodb.Table('USGSEarthquakeEvents')
+table = dynamodb.Table('earthquake')
 sns = boto3.client('sns')
 
-SNS_ARN = "arn:aws:sns:ap-south-1:xxxx:Earthquake"
+SNS_ARN = "arn:aws:sns:us-east-1:279199662648:earthquake:d388c60e-8a0e-42f1-a5d5-76ef5e03045d"
 
 URL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson"
 
